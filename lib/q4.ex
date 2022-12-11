9c4c2d59f6065ed7024bb8df61357197a89393bd
vtno <tino@vtno.me>
2022-12-12 03:17:15 +0700

    feat: 4 part 2

--- lib/q4.ex
+++ lib/q4.ex
@@ -4,16 +4,16 @@ defmodule Q4 do
   """
 
   @doc """
-  Q4.run will solve question 4 part 1
+  Q4.run will solve question 4
   """
-  def run(file_name \\ "q4.txt") do
+  def run(file_name \\ "q4.txt", partial? \\ false) do
     case File.read(file_name) do
       {:ok, data} ->
         data
         |> String.split("\n", trim: true)
         |> Enum.map(&String.split(&1, ","))
-        |> Enum.map(&compare(Enum.at(&1, 0), Enum.at(&1, 1)))
-        |> Enum.count(fn list -> length(list) == 0 end)
+        |> Enum.map(&compare(Enum.at(&1, 0), Enum.at(&1, 1), partial?))
+        |> Enum.sum
     end
   end
 
@@ -34,12 +34,32 @@ defmodule Q4 do
   The smaller one will be subtracted by the larger one which resulted
   in an empty list if it is fully covered.
   """
-  def compare(src, target) do
+  def compare(src, target, partial? \\ false) do
     src_list = to_list(src)
     target_list = to_list(target)
     cond do
-      length(src_list) >= length(target_list) -> target_list -- src_list
-      length(src_list) < length(target_list) ->  src_list -- target_list
+      length(src_list) >= length(target_list) ->
+        compute_compare_result(target_list -- src_list, target_list, partial?)
+      length(src_list) < length(target_list) ->
+        compute_compare_result(src_list -- target_list, src_list, partial?)
+    end
+  end
+
+  defp compute_compare_result(result, subtracted_list, partial?) do
+    if length(result) == 0 do
+      1
+    else
+      compute_partial_compare_result(result, subtracted_list, partial?)
+    end
+  end
+
+  defp compute_partial_compare_result(result, subtracted_list, partial?) do
+    # if partial? option is provided check if the subtraction resulted in the same
+    # list as the subtrahend which means nothing was subtracted
+    if partial? && result != subtracted_list do
+      1
+    else
+      0
     end
   end
 end
--- test/q4_test.exs
+++ test/q4_test.exs
@@ -21,7 +21,7 @@ defmodule Q4Test do
   end
 
   describe "compare" do
-    test "subtract two list by using the larger one" do
+    test "return 1 if range is fully covered when partial option is false" do
       inputs = [
         ["1-3", "2-4"],
         ["1-3", "4-5"],
@@ -31,15 +31,37 @@ defmodule Q4Test do
       ]
 
       expected = [
-        [4],
-        [4, 5],
-        [],
-        [],
-        []
+        0,
+        0,
+        1,
+        1,
+        1
       ]
 
       inputs
       |> Enum.map(&Q4.compare(Enum.at(&1, 0), Enum.at(&1, 1)))
+      |> (&assert(&1 == expected)).()
+    end
+
+    test "return 1 if range is fully covered when partial option is true" do
+      inputs = [
+        ["1-3", "2-4"],
+        ["1-3", "4-5"],
+        ["1-3", "1-1"],
+        ["1-1", "1-3"],
+        ["1-1", "1-1"]
+      ]
+
+      expected = [
+        1,
+        0,
+        1,
+        1,
+        1
+      ]
+
+      inputs
+      |> Enum.map(&Q4.compare(Enum.at(&1, 0), Enum.at(&1, 1), true))
       |> (&assert(&1 == expected)).()
     end
   end
@@ -47,6 +69,10 @@ defmodule Q4Test do
   describe "run" do
     test "count number of fully covered pair of ranges" do
       assert Q4.run("q4_test_fixture.txt") == 2
+    end
+
+    test "count number of partially covered pair of ranges" do
+      assert Q4.run("q4_test_fixture.txt", true) == 4
     end
   end
 end
