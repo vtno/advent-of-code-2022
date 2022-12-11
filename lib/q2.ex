ec050019de3a2585ecf9175779d8f7e9df4aa904
vtno <tino@vtno.me>
2022-12-11 18:07:55 +0700

    feat: q2 part 2

--- lib/q2.ex
+++ lib/q2.ex
@@ -16,6 +16,7 @@ defmodule Q2 do
   Round score
   0 if you lost, 3 if the round was a draw, and 6 if you won
 
+  # Part 1
   Pair
 
   A X = draw + rock = 3 + 1 = 4
@@ -29,6 +30,21 @@ defmodule Q2 do
   C X = win + rock = 6 + 1 = 7
   C Y = lose + paper = 0 + 2 = 2
   C Z = draw + scissors = 3 + 3 = 6
+
+  # Part 2
+  X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+
+  A X = rock + lose = scissors = 3 + 0 = 3
+  A Y = rock + draw = rock = 1 + 3 = 4
+  A Z = rock + win = paper = 2 + 6 = 8
+
+  B X = paper + lose = rock = 1 + 0 = 1
+  B Y = paper + draw = paper = 2 + 3 = 5
+  B Z = paper + win = scissors =  3 + 6 = 9
+
+  C X = scissors + lose = paper = 2 + 0 = 2
+  C Y = scissors + draw = scissors =  3 + 3 = 6
+  C Z = scissors + win = rock = 1 + 6 = 7
   """
 
   @score %{
@@ -43,16 +59,32 @@ defmodule Q2 do
     "C Z" => 6
   }
 
+  @score_2 %{
+    "A X" => 3,
+    "A Y" => 4,
+    "A Z" => 8,
+    "B X" => 1,
+    "B Y" => 5,
+    "B Z" => 9,
+    "C X" => 2,
+    "C Y" => 6,
+    "C Z" => 7
+  }
+
   @doc """
   Q2.run() will solve the second question
   """
-  def run(file_name \\ "q2.txt") do
+  def run(file_name, score_getter) do
     case File.read(file_name) do
-      {:ok, data} -> data |> String.split("\n", trim: true) |> Enum.map(&get_score(&1)) |> Enum.sum
+      {:ok, data} -> data |> String.split("\n", trim: true) |> Enum.map(score_getter) |> Enum.sum
     end
   end
 
-  defp get_score(scenario) do
+  def get_score(scenario) do
     @score[scenario]
+  end
+
+  def get_score2(scenario) do
+    @score_2[scenario]
   end
 end
--- test/q2_test.exs
+++ test/q2_test.exs
@@ -3,8 +3,12 @@ defmodule Q2Test do
   doctest Q2
 
   describe "run" do
-    test "calculate the score correctly based on the rule" do
-      assert Q2.run("q2_test_fixture.txt") == 15
+    test "calculate the score correctly based on the rule of part 1" do
+      assert Q2.run("q2_test_fixture.txt", &Q2.get_score(&1)) == 15
+    end
+
+    test "calculate the score correctly based on the rule of part 2" do
+      assert Q2.run("q2_test_fixture.txt", &Q2.get_score2(&1)) == 12
     end
   end
 end
